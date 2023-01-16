70919435bbb30dfc0a7bdac737542dff7e15294a
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-01-16 11:41:27 +0000

    final sub

--- src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/ImInSpAInWithoutTheS.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/ImInSpAInWithoutTheS.java
@@ -14,28 +14,28 @@ public class ImInSpAInWithoutTheS extends Player {
         super(counter, ImInSpAInWithoutTheS.class.getName());
     }
 
-    public int validRandomMove(Board board, List<Integer> columnsToPickFrom){
+
+    public int validRandomMove(Board board, List<Integer> columnsToPickFrom) {
         int randomMove;
         do {
             randomMove = columnsToPickFrom.get(ThreadLocalRandom.current().nextInt(columnsToPickFrom.size()));
-        } while (board.hasCounterAtPosition(new Position(randomMove, board.getConfig().getHeight()-1)));
+        } while (board.hasCounterAtPosition(new Position(randomMove, board.getConfig().getHeight() - 1)));
         return randomMove;
     }
 
-    public int mostCentralValidMove(Board board, List<Integer> columnsToPickFrom){
-        int middleColumnOnBoard = board.getConfig().getWidth()/2;
+    public int mostCentralValidMove(Board board, List<Integer> columnsToPickFrom) {
+        int middleColumnOnBoard = board.getConfig().getWidth() / 2;
         int closest = 0;
-        int distance = Math.abs(columnsToPickFrom.get(0)-middleColumnOnBoard);
-        for (int number=1; number<columnsToPickFrom.size(); number++){
-            int currentDistance = Math.abs(columnsToPickFrom.get(number)-middleColumnOnBoard);
-            if (currentDistance <  distance){
+        int distance = Math.abs(columnsToPickFrom.get(0) - middleColumnOnBoard);
+        for (int number = 1; number < columnsToPickFrom.size(); number++) {
+            int currentDistance = Math.abs(columnsToPickFrom.get(number) - middleColumnOnBoard);
+            if (currentDistance < distance) {
                 closest = number;
                 distance = currentDistance;
             }
         }
         return columnsToPickFrom.get(closest);
     }
-
 
 
     @Override
@@ -48,14 +48,21 @@ public class ImInSpAInWithoutTheS extends Player {
                 return winningMove;
             }
             Integer blockingAWin = slayIAnalyser.winningColumn(board, getCounter().getOther());
-            if (blockingAWin != null){
+            if (blockingAWin != null) {
                 return blockingAWin;
             }
+            List<Integer> builds3InARow = slayIAnalyser.getMovesThatExtendATwo(board, getCounter(), propaGoodMoves);
+            if (!builds3InARow.isEmpty()) {
+                return mostCentralValidMove(board, builds3InARow);
+            }
+            List<Integer> blocksFor2InARow = slayIAnalyser.getMovesThatExtendATwo(board, getCounter().getOther(), propaGoodMoves);
+            if (!blocksFor2InARow.isEmpty()) {
+                return mostCentralValidMove(board, blocksFor2InARow);
+            }
             return validRandomMove(board, propaGoodMoves);
-        } catch(Exception exception){
+        } catch (Exception exception) {
             return validRandomMove(board, propaGoodMoves);
         }
-
     }
 }
 
--- src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/UrDaSellsAIvon.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/politicallyconnect/UrDaSellsAIvon.java
@@ -10,28 +10,27 @@ public class UrDaSellsAIvon extends Player {
         super(counter, UrDaSellsAIvon.class.getName());
     }
 
-    public int validRandomMove(Board board, List<Integer> columnsToPickFrom){
+    public int validRandomMove(Board board, List<Integer> columnsToPickFrom) {
         int randomMove;
         do {
             randomMove = columnsToPickFrom.get(ThreadLocalRandom.current().nextInt(columnsToPickFrom.size()));
-        } while (board.hasCounterAtPosition(new Position(randomMove, board.getConfig().getHeight()-1)));
+        } while (board.hasCounterAtPosition(new Position(randomMove, board.getConfig().getHeight() - 1)));
         return randomMove;
     }
 
-    public int mostCentralValidMove(Board board, List<Integer> columnsToPickFrom){
-        int middleColumnOnBoard = board.getConfig().getWidth()/2;
+    public int mostCentralValidMove(Board board, List<Integer> columnsToPickFrom) {
+        int middleColumnOnBoard = board.getConfig().getWidth() / 2;
         int closest = 0;
-        int distance = Math.abs(columnsToPickFrom.get(0)-middleColumnOnBoard);
-        for (int number=1; number<columnsToPickFrom.size(); number++){
-            int currentDistance = Math.abs(columnsToPickFrom.get(number)-middleColumnOnBoard);
-            if (currentDistance <  distance){
+        int distance = Math.abs(columnsToPickFrom.get(0) - middleColumnOnBoard);
+        for (int number = 1; number < columnsToPickFrom.size(); number++) {
+            int currentDistance = Math.abs(columnsToPickFrom.get(number) - middleColumnOnBoard);
+            if (currentDistance < distance) {
                 closest = number;
                 distance = currentDistance;
             }
         }
         return columnsToPickFrom.get(closest);
     }
-
 
 
     @Override
@@ -44,15 +43,20 @@ public class UrDaSellsAIvon extends Player {
                 return winningMove;
             }
             Integer blockingAWin = slayIAnalyser.winningColumn(board, getCounter().getOther());
-            if (blockingAWin != null){
+            if (blockingAWin != null) {
                 return blockingAWin;
             }
-            return validRandomMove(board, propaGoodMoves);
-        } catch(Exception exception){
+            List<Integer> builds3InARow = slayIAnalyser.getMovesThatExtendATwo(board, getCounter(), propaGoodMoves);
+            if (!builds3InARow.isEmpty()) {
+                return mostCentralValidMove(board, builds3InARow);
+            }
+            List<Integer> blocksFor2InARow = slayIAnalyser.getMovesThatExtendATwo(board, getCounter().getOther(), propaGoodMoves);
+            if (!blocksFor2InARow.isEmpty()) {
+                return mostCentralValidMove(board, blocksFor2InARow);
+            }
+            return mostCentralValidMove(board, slayIAnalyser.getMinValidVacantY(board, propaGoodMoves));
+        } catch (Exception exception) {
             return validRandomMove(board, propaGoodMoves);
         }
-
     }
 }
-
-
